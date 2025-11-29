66ad1c8a80682819aada69d4916eb159e01b85db
FERELAR <gosudarev234@gmail.com>
2025-11-29 23:56:39 +0300

    Update meteoCoder.js

--- meteoCoder.js
+++ meteoCoder.js
@@ -1,3 +1,4 @@
+let currentEncodeExercise = null;
 let trainerStats = JSON.parse(localStorage.getItem('trainerStats') || '{"level":1,"totalDecoded":0,"correctDecoded":0,"sessionDecoded":0,"sessionCorrect":0,"errorsByType":{"metar":0,"kn01":0,"taf":0,"gamet":0,"sigmet":0,"warep":0,"kn04":0,"airmet":0}}');
 let currentPracticeCode = null;
 let hintStep = 0;
@@ -5,7 +6,7 @@ let hintStep = 0;
 
 let appSettings = JSON.parse(localStorage.getItem('meteoCoderSettings') || '{"theme":"light","fontSize":"normal","animations":true}');
 
-const WEATHER_CODES1 = {
+const WEATHER_CODES = {
   'MI': 'мелкий',     'BC': 'пятнами',     'PR': 'частично',     'DR': 'низкий перенос', 'BL': 'высокий перенос',
   'SH': 'ливневый',   'TS': 'гроза',       'FZ': 'переохлаждённый/замерзающий',
   'DZ': 'морось',     'RA': 'дождь',       'SN': 'снег',         'SG': 'снежные зерна', 'IC': 'ледяные кристаллы',
@@ -15,13 +16,13 @@ const WEATHER_CODES1 = {
   'PO': 'пыльный вихрь', 'SQ': 'шквал',    'FC': 'воронка (торнадо)', 'SS': 'песчаная буря', 'DS': 'пыльная буря'
 };
 
-const CLOUD_TYPES1 = {
+const CLOUD_TYPES = {
   'SKC': 'ясно', 'CLR': 'ясно (автомат)', 'NSC': 'нет значимой облачности', 'FEW': 'малооблачно (1–2/8)',
   'SCT': 'рассеянная облачность (3–4/8)', 'BKN': 'значительная облачность (5–7/8)', 'OVC': 'сплошная облачность (8/8)',
   'VV': 'вертикальная видимость', '///': 'облачность не определяется'
 };
 
-const CLOUD_SUFFIX1 = { 'CB': 'кучево-дождевые (Cb)', 'TCU': 'мощные кучевые (Tcu)' };
+const CLOUD_SUFFIX = { 'CB': 'кучево-дождевые (Cb)', 'TCU': 'мощные кучевые (Tcu)' };
 
 // ====================== ТОКЕНИЗАТОР ======================
 const TOKENS = {
@@ -81,7 +82,7 @@ function decodeWeather(code) {
   const obscuration = code.match(/(BR|FG|FU|VA|DU|SA|HZ|PY)*/)[0];
   const other = code.match(/(PO|SQ|FC|SS|DS)*/)[0];
 
-  const translate = (str) => str ? str.match(/.{2}/g).map(c => WEATHER_CODES1[c] || c).join(' ') : '';
+  const translate = (str) => str ? str.match(/.{2}/g).map(c => WEATHER_CODES[c] || c).join(' ') : '';
 
   result += intensity;
   result += translate(descriptors) + ' ';
@@ -206,9 +207,9 @@ class MetarParser extends BaseParser {
       if (m[1] === 'VV') {
         this.result.push(`Вертикальная видимость: ${parseInt(m[2]) * 30} м`);
       } else {
-        const cover = CLOUD_TYPES1[m[1]] || m[1];
+        const cover = CLOUD_TYPES[m[1]] || m[1];
         const height = m[2] !== '///' ? `${parseInt(m[2]) * 30} м (${parseInt(m[2]) * 100} ft)` : 'неизвестна';
-        const special = m[3] && m[3] !== '///' ? CLOUD_SUFFIX1[m[3]] : '';
+        const special = m[3] && m[3] !== '///' ? CLOUD_SUFFIX[m[3]] : '';
         this.result.push(`Облачность: ${cover}, высота ${height}${special ? ', ' + special : ''}`);
       }
     }
@@ -603,4 +604,5 @@ function parseWarep(code) { return 'Парсер WAREP в разработке';
 function parseKn04(code) { return 'Парсер КН-04 в разработке'; }
 
 function parseAirmet(code) { return 'Парсер AIRMET в разработке'; }
+
 
